1fdf95a2810d050a52ca78aa0b3b931ea85db2cb
ramonalmonte <61474343+ramonalmonte@users.noreply.github.com>
2020-02-25 14:21:51 -0500

    Add files via upload

---
+++ my_scripts.js
@@ -0,0 +1,55 @@
+    $("#send").click(function(){
+        var msg = $("#text-box").val();
+        
+        $("#this-message").text(msg);
+
+        var markup = "<tr><td>" + msg + "</td></tr>";
+        
+       /* $("table tbody").append(msg);*/
+
+       document.getElementById("demo").innerHTML = testingFunction;
+        
+        $("#text-box").val("");
+    });
+
+
+    function ALFunction(){
+    	 var simpleGreeding = ["Good Morning", "Good Afternoon", "Good Night", "What are you doing this Weekedend?", "How was your day", 
+    	 "I feel you bro", ]
+    	 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"]
+    	 var classicReplay = ["I am Really good", "Your welcome","Bye"]
+
+    
+
+    } 
+
+    function TestingFunction(){
+
+    <!DOCTYPE html>
+	<html>
+	<body>
+	 var DayOfWeek = getDay()
+
+	<h1>My First Web Page</h1>
+	<p>My First Paragraph</p>
+
+	<p id="demo"></p>
+
+
+
+	<script>
+	document.write(DayOfWeek)
+
+	</script>
+
+	</body>
+	</html>
+
+	return DayOfWeek;
+    }
+
+
+
+
+      
+
